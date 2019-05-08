166df2c4869dd159a94469a51eebd333c7a15cc8
Ali Stvt <alistvt@gmail.com>
2019-05-08 15:10:10 +0430

    creates model merchant

--- merchants/models.py
+++ merchants/models.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+class Merchant(User):
+    pass
+
+    class Meta:
+        verbose_name = _("Merchant")
+        verbose_name_plural = _("Merchants")
+        ordering = ['last_name']
+
+    def __str__(self):
+        return '{name}'.format(name=self.first_name + ' ' + self.last_name)
