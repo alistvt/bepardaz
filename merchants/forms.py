915a01b98d22afc18bf8d61b0368ee1290d34b02
Ali Stvt <alistvt@gmail.com>
2019-05-12 18:12:24 +0430

    creates merchant creation form

---
+++ merchants/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext_lazy as _
+
+from merchants.models import Merchant
+
+
+class MerchantCreationForm(UserCreationForm):
+    """
+    A form that creates a merchant, with no privileges, from the given username and
+    password.
+    """
+    class Meta:
+        model = Merchant
+        fields = ('phone', 'email', 'password1', 'password2', 'first_name', 'last_name', )
+
+    def save(self, commit=True):
+        merchant = super().save(commit=False)
+        merchant.username = None
+        merchant.save()
+        return merchant
