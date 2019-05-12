dd74bfdc43d36bd050e05fee38bf3de3fdf503ad
Ali Stvt <alistvt@gmail.com>
2019-05-12 17:24:39 +0430

    changes phone field and adds a validation to it

--- merchants/models.py
+++ merchants/models.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 
 
 class Merchant(User):
-    # todo (ask): the best way to store phone number
-    phone = models.CharField(max_length=20, verbose_name=_('phone number'))
+    phone_number = models.CharField(max_length=20,
+                                    verbose_name=_('phone number'),
+                                    validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                                               message=_("entered phone number is invalid."))])
 
     class Meta:
         verbose_name = _("merchant")
@@ -18,10 +21,11 @@ class Merchant(User):
         ordering = ['last_name']
 
     def __str__(self):
-        return '{name}'.format(name=self.first_name + ' ' + self.last_name)
+        return '{first_name} {last_name}'.format(fisrt_name=self.first_name, last_name=self.last_name)
 
 
 class PaymentForm(models.Model):
+    # todo change  to owner
     creator = models.ForeignKey(Merchant, related_name='payment_forms', on_delete=models.CASCADE, verbose_name=_('creator'))
     title = models.CharField(max_length=255, verbose_name=_('title'))
     # todo (ask): for blank and null value
@@ -29,6 +33,7 @@ class PaymentForm(models.Model):
     price = models.PositiveIntegerField(verbose_name=_('price'))
     link = models.CharField(max_length=36, verbose_name=_('link'))
     # times that link can be payed. 0 means infinitely.
+    # todo change name to ..
     payable_times = models.PositiveSmallIntegerField(default=0, verbose_name=_('payable times'))
     created_date = models.DateTimeField(auto_now_add=True, verbose_name=_('creation cate'))
 
