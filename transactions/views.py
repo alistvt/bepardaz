59eedefff41c969113d8f3c8ea1b1c10aa090d12
Ali Stvt <alistvt@gmail.com>
2019-05-21 18:49:56 +0430

    creates payment form frontend
    css needed

--- transactions/views.py
+++ transactions/views.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from merchants.models import PaymentForm
+
 # Create your views here.
 
 def payment_form_view(request, link):
-    pass
+    payment_form =  PaymentForm.objects.get(link=link)
+    return render(request, 'payment_form.html', {'pf': payment_form})
